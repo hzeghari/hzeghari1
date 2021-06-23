926a81b456ad395626cf130030e1bc737634fd6b
Hamza Zeghari <zeghari.hamzaa@gmail.com>
2021-06-23 21:52:00 +0100

    first commit 4 & footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,7 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/hzeghari/hzeghari1">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Designed &amp; Built by Hamza Zeghari</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
